aa236f59a9772ee8e5f5f08448269e8bfb7feaeb
roj21008 <roj21008@byui.edu>
2022-03-09 22:14:50 -0500

    initial commit

---
+++ cycle/constants.py
@@ -0,0 +1,17 @@
+from game.shared.color import Color
+
+
+COLUMNS = 40
+ROWS = 20
+CELL_SIZE = 15
+MAX_X = 900
+MAX_Y = 600
+FRAME_RATE = 15
+FONT_SIZE = 15
+CAPTION = "Snake"
+SNAKE_LENGTH = 4
+WHITE = Color(255, 255, 255)
+RED = Color(255, 0, 0)
+YELLOW = Color(255, 255, 0)
+GREEN = Color(150, 255, 0)
+BLUE = Color(59, 131,189)
